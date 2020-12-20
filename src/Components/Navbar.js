9d9d4dc69ab33b7cb14113113ddd91c154c04dfb
igreen1 <iangreen2.00@gmail.com>
2020-12-20 15:49:20 -0800

    Functional Navbar using the Sursum logo and two links to / and /about More can be added easily

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import { React, useState } from "react";
+import { withRouter } from "react-router-dom";
+import "./Navbar.css";
+
+import { FaAlignRight } from "react-icons/fa";
+import sursumLogo from '../images/sursumShield.png'
 
 function Navbar(props) {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggle = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div>
-      <img src={sursum-logo}
+      <div id="Header">
+        <img src={sursumLogo} id="NavbarLogo" alt="Logo" href="/" />
+        <div id="Navbar">
+          <button onClick={() => toggle()} id="ExpandNavbar">
+            <FaAlignRight />
+          </button>
+          <ul className={expanded ? "NavbarList show-nav" : "NavbarList"}>
+            <li className="NavbarListItem">
+              <a className="InlineLink" href="/">
+                Home
+              </a>
+            </li>
+            <li className="NavbarListItem">
+              <a className="InlineLink" href="/about">
+                About
+              </a>
+            </li>
+          </ul>
+        </div> {/*End navbar*/}
+      </div> {/* End header*/ }
+
+      <div className="Spacer">{/* Spacing */}</div>
     </div>
-  )
+  );
 }
+
+export default withRouter(Navbar);
