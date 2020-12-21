7c8d5271ddb48f3ac5af5ddc520f3cde0841b541
igreen1 <iangreen2.00@gmail.com>
2020-12-20 17:01:09 -0800

    Added routing and pages (Homepage)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import AboutSursum from './Pages/AboutSursum'
 import Navbar from './Components/Navbar'
 import ImageSlider from './Components/ImageSlider'
 import './App.css';
+import Homepage from './Pages/Homepage'
 
 
 function App() {
   return (
     <div>
     <Router>
-      <Navbar />
-      <ImageSlider />
-      <AboutSursum />
+      <Navbar/>
+      <Route exact path='/'>
+        <Homepage />
+      </Route>
+      <Route path='/about'>
+        <AboutSursum/>
+      </Route>
     </Router>
     </div>
   );
---
+++ src/Pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ImageSlider from './../Components/ImageSlider'
+
+
+
+function Homepage(){
+  return (
+  <React.Fragment>
+    <ImageSlider/>
+  </React.Fragment>
+  )
+}
+
+
+export default Homepage
