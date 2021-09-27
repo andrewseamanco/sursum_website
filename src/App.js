7a2ccdaceb870e5717857452140f54fcd8e15b1f
hallegv <hallevogelpohl@gmail.com>
2021-09-27 14:04:39 -0700

    fixed padding in App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Navbar from './Components/Navbar'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import "./App.css";
 
-import Homepage from './Pages/Homepage'
-import Events from './Pages/Events'
-import Join from './Pages/Join'
-import Contact from './Pages/Contact'
+import Homepage from "./Pages/Homepage";
+import Events from "./Pages/Events";
+import Join from "./Pages/Join";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
     <div>
-    <p>HELLO</p>
-    <p>This is working</p>
-    
-    <Router>
-      <Navbar/>
-      <Route exact path='/'>
-        <Homepage />
-      </Route>
-      <Route path='/events'>
-        <Events />
-      </Route>
-      <Route path='/join'>
-        <Join />
-      </Route>
-      <Route path='/contact'>
-        <Contact />
-      </Route>
-    </Router>
+      <Router>
+        <Navbar />
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/events">
+          <Events />
+        </Route>
+        <Route path="/join">
+          <Join />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Router>
     </div>
   );
 }
